95cc525f49862d238996edbca9691f0178d599be
robtyers <rob.tyers@gmail.com>
2016-09-19 19:55:26 +0100

    Added Convenience and Server request objects

--- RippleSharp.Rippled/Interfaces/IRippledClient.cs
+++ RippleSharp.Rippled/Interfaces/IRippledClient.cs
@@ -6,9 +6,11 @@ namespace RippleSharp.Rippled.Interfaces
     public interface IRippledClient
     {
         AccountInformationRequests AccountInformation { get; }
+        ConvenienceRequests Convenience { get; }
         LedgerRequests Ledger { get; }
+        ServerRequests Server { get; }
         TransactionRequests Transactions { get; }
-      
+
         Task<bool> ConnectedAsync();
         Task<string> PostAsync(IRippledRequest request);
     }
